0dd26382444158a26ac4c6fc6b1cd8cfc5f5d5f2
SeoYeonnn <ldd6407@naver.com>
2024-02-21 22:12:35 +0900

    test

--- src/main/java/com/zerobase/fastlms/FastlmsApplication.java
+++ src/main/java/com/zerobase/fastlms/FastlmsApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FastlmsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FastlmsApplication.class, args);
+		SpringApplication.run(FastlmsApplication.class, args);   
 	}
 
 }
